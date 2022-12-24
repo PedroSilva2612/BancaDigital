12026922f27baafbdf7b346a537377e5eff8a6f1
Pedro Silva <84540561+PedroSilva2612@users.noreply.github.com>
2022-12-24 16:56:09 -0400

    update 1.1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const boxes = document.querySelectorAll('.box');
 
 const checkBoxes = () => {
-    const triggerBottom = (window.innerHeight / 3) * 3;
+    const triggerBottom = (window.innerHeight / 6) * 4;
     boxes.forEach((box) => {
         const boxTop = box.getBoundingClientRect().top;
         if( boxTop < triggerBottom ) box.classList.add('show');
